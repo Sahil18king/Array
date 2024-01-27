8fe157940d2a846387008cc2ca842f361d39a713
SAHIL SONI <133500990+Sahil18king@users.noreply.github.com>
2024-01-27 11:31:13 +0530

    Update SubArray.java
    Subarray code using array

--- SubArray.java
+++ SubArray.java
@@ -1,21 +1,17 @@
-import java.util.*;
-public class JavaBasics{
-    public static void Subarray(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            int s =i;
-            for(int j=i; j<arr.length; j++){
-                int e =j;
-                for(int k=s; k<=e; k++){
-                    System.out.print(arr[k]);
-                }
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-        
-    }
-    public static void main(String args[]){
-        int arr[]={2,4,6,8,10};
+public class Javabasics{
+    public static void main(String args[]) {
+        int arr[] ={2,4,6,8,10};
         Subarray(arr);
     }
+    public static void Subarray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i; j<arr.length; j++){
+                for(int k=i ; k<=j; k++){
+                    System.out.print(arr[k]+",");
+                }
+                System.out.println();
+            }
+        }
+    }
+    
 }
